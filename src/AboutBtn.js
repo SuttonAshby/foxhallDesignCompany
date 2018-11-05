c06b816566d5b3c7a8cc90df5e78edd9de900e12
Sutton Ashby <korim_fidelis@yahoo.com>
2018-11-05 14:13:16 -0500

    added about button

---
+++ src/AboutBtn.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+
+const AboutBtn = props => {
+    return (
+        <Button 
+            variant="outlined" 
+            >
+        About
+      </Button>
+    )
+}
+
+export default AboutBtn
--- src/Navbar.js
+++ src/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from './logo.jpg'
 import ProductsBtn from './ProductsBtn.js'
 import ContactBtn from './ContactBtn.js'
+import AboutBtn from './AboutBtn.js'
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import { Grid } from '@material-ui/core';
@@ -26,6 +27,9 @@ const Navbar = props => {
                         </Grid>
                         <Grid item>
                             <ContactBtn/>
+                        </Grid>
+                        <Grid item>
+                            <AboutBtn/>
                         </Grid>
                     </Grid>
                 </Toolbar>
