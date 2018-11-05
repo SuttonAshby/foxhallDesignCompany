513643634fab7723cc27b3369b959da21f9c7589
Sutton Ashby <korim_fidelis@yahoo.com>
2018-11-05 15:51:17 -0500

    added json to map splash products

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,23 @@
 import React from 'react';
-import Navbar from './Navbar';
+import ProductSplash from './ProductSplash.js';
+import data from './data.json';
 
-const HomePage = () => {
+const HomePage = (props) => {
+    
+    const productLines = data.map((lines) => {
+        return <ProductSplash 
+            key={lines.key}
+            id={lines.id}
+            image={lines.image}
+            title={lines.title}
+            blurb={lines.blurb}
+            />
+
+    })
+
     return (
         <div>
-            Home
+            {productLines}
         </div>
     )
 }
--- src/ProductSplash.js
+++ src/ProductSplash.js
@@ -1,2 +1,17 @@
 import React from 'react';
 
+const ProductSplash = (props) => {
+    return (
+        <div
+            className="splash"
+            key={props.key}
+            id={props.id}>
+        <img src={props.image} />
+        <h1 className="productLine">{props.title}</h1>
+        <p>{props.blurb}</p>    
+        </div>
+            
+    )
+}
+
+export default ProductSplash;
