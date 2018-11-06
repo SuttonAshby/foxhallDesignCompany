374894e77e6f4213f36fb543aa8d02f8312827a1
Sutton Ashby <korim_fidelis@yahoo.com>
2018-11-05 19:24:52 -0500

    trying to center image

--- src/ProductSplash.js
+++ src/ProductSplash.js
@@ -1,24 +1,29 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const ProductSplash = (props) => {
     return (
-        <div
+        <Grid container
+            justify="center" 
             className="splash"
             key={props.key}
             id={props.id}>
-        <img 
-            align="center"
-            src={props.image} />
-        <Typography 
-            variant="title"
-            align="center"
-            className="productLine">{props.title}</Typography>
-        <Typography
-            variant="body1"
-            align="center">{props.blurb}</Typography>    
-        </div>
-            
+            <Grid item xs={12} >
+                <img width="50%" src={props.image} />
+            </Grid>
+            <Grid item xs={12}>
+                <Typography 
+                    variant="title"
+                    align="center"
+                    className="productLine">{props.title}
+                </Typography>
+                <Typography
+                    variant="body1"
+                    align="center">{props.blurb}
+                </Typography>    
+            </Grid>
+        </Grid>           
     )
 }
 
