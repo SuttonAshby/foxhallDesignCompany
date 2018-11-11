dcdaeff6bca61bab4491e5be8d16c02bef8d5600
Sutton Ashby <korim_fidelis@yahoo.com>
2018-11-11 17:56:33 -0500

    updating about page

--- src/AboutPage.js
+++ src/AboutPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
-
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 
 const AboutPage = () => {
     return (
-        <div>
-            About
-        </div>
+        <Grid container>
+            <Grid item>
+                <Typography
+                    variant="h5"
+                    align="left">
+                    About Us    
+                </Typography>
+                <Typography
+                    variant="body1"
+                    align="left">
+                    Lorum Ipsum
+                </Typography>
+            </Grid>
+        </Grid>
     )
 }
 
