3634fe4e83acd90b8fddf261ee9c1fcc098db5b5
Sutton Ashby <korim_fidelis@yahoo.com>
2018-11-14 23:55:25 -0500

    started ImageCarousel

---
+++ src/components/ImageCarousel.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Switch from '@material-ui/core/Switch';
+import Paper from '@material-ui/core/Paper';
+import Slide from '@material-ui/core/Slide';
+
+
+class ImageCarousel extends React.Component {
+    state = {
+
+    }
+    render(){
+    }
+}
+
+export default ImageCarousel;
--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -3,7 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 
 const ProductDisplay = (props) => {
-    
+        
 }
 
 export default ProductDisplay
