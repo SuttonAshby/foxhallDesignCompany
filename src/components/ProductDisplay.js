3bb73cbb653f8297fb62355a36a624a83d6a57c4
Sutton Ashby <korim_fidelis@yahoo.com>
2018-11-15 16:06:07 -0500

    adding to product display

--- src/components/ProductDisplay.js
+++ src/components/ProductDisplay.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import ImageCarousel from './ImageCarousel';
 
 const ProductDisplay = (props) => {
-        
+      <Grid container>
+        <Grid item xs="12">
+            <ImageCarousel />
+        </Grid>
+            
+        <Grid item xs="12">
+        <Typography>
+                {props.about}
+            </Typography>
+        </Grid>
+      </Grid>  
 }
 
 export default ProductDisplay
