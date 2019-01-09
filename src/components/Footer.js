089af3f6dcef75eb5c5800e77b9875c5b6a27bfd
Sutton Ashby <korim_fidelis@yahoo.com>
2019-01-09 00:41:02 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -19,7 +19,7 @@ const Footer = props => {
     <div style={styles.footer}>
             <Typography
                 style={styles.text}>
-                    &#169; Foxhall Design Company 2018 | Website by Sutton Ashby
+                    &#169; Foxhall Design Company 2018 | Website by Sutton Ashby | Photography by Nick Bohaska
             </Typography>
     </div>
     )
