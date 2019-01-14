99cfdbb62b220345a3f0c26eb9f0f7616b77edb9
Sutton Ashby <korim_fidelis@yahoo.com>
2019-01-14 03:02:17 -0500

    more responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 
 const styles = {
@@ -6,6 +6,12 @@ const styles = {
         marginTop: 20,
         textAlign: "center",
         height: 30,
+        background: "#606060",
+    },
+    footerSmall: {
+        marginTop: 20,
+        textAlign: "center",
+        height: 60,
         background: "#606060",
     },
     text: {
@@ -16,6 +22,45 @@ const styles = {
 
 // need to adjust footer when under 600px
 
+class Footer extends Component {
+    state = {
+        lessThan: false
+    }
+    
+    updateFooter(){
+        if(window.innerWidth < 650) {
+            this.setState({lessThan: true});
+        } else {
+            this.setState({lessThan: false});
+        }
+    }
+
+    componentDidMount(){
+        this.updateFooter()
+        window.addEventListener("resize", this.updateFooter.bind(this))
+    }
+
+    componentWillUnmount(){
+        window.removeEventListener("resize", this.updateFooter.bind(this))    
+    }
+
+    render(){
+        return (
+            this.state.lessThan ? <div style={styles.footerSmall}>
+            <Typography
+                style={styles.text}>
+                    &#169; Foxhall Design Company 2018 <br /> Website by Sutton Ashby | Photography by George Bohaska
+            </Typography></div> : <div style={styles.footer}>
+            <Typography
+                style={styles.text}>
+                    &#169; Foxhall Design Company 2018 Website by Sutton Ashby | Photography by George Bohaska
+            </Typography></div>
+        )
+    }
+    
+}
+
+/*
 const Footer = props => {
     return (
     <div style={styles.footer}>
@@ -26,5 +71,6 @@ const Footer = props => {
     </div>
     )
 }
+*/
 
 export default Footer;
